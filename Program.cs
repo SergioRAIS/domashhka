e06610d01735226822a29eaf151bc3cef9035d6e
SergioRAIS <130254963+SergioRAIS@users.noreply.github.com>
2023-06-18 22:57:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int chislo = ReadInt("Введите число: ");
+int count = chislo.ToString().Length;
+Console.Write(ChiSlo(chislo, count));
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int ChiSlo(int a, int b)
+{
+int chiselo = 0;
+if (b < 3)
+{
+Console.Write("Третьей цифры нет ---->");
+}
+else
+{
+int z = 1;
+for (int i = b; i > 3; i--)
+{
+z = z * 10;
+}
+
+chiselo = (a / z) % 10;
+}
+return chiselo;
+}
