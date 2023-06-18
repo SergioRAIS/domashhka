a92cbfee4094be0c71d3fbecf218fe60b85ba98e
SergioRAIS <130254963+SergioRAIS@users.noreply.github.com>
2023-06-18 23:06:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int dayNumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(DniNedely(dayNumber));
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+string DniNedely(int o)
+{
+if (o > 0 && o < 8)
+{
+if (o == 7 || o == 6)
+{
+Console.Write(+ o + "---> да");
+}
+else
+{
+Console.Write(+ o + "---> нет");
+}
+}
+else
+{
+Console.Write("Ведите число от 1 до 7 повторно");
+}
+return "";
+}
